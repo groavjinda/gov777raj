17b3b52a3103c57433d4db7c74354153683f2c49
Deepa <Deepa@GOVINDARAJ>
2016-08-16 21:47:45 +0530

    New module added

---
+++ StudentDemo/StudentDemo/Controllers/StudentController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using StudentDemo.Models;
+
+namespace StudentDemo.Controllers
+{
+    public class StudentController : Controller
+    {
+        // GET: Student
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Student()
+        {
+
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Student(StudentModel obj)
+        {
+            ViewBag.Msg = "The student having name " + obj.Name + " of class " + obj.Class + " added";
+            return View();
+        }
+
+    }
+}
---
+++ StudentDemo/StudentDemo/Models/StudentModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace StudentDemo.Models
+{
+    public class StudentModel
+    {
+        [Required(ErrorMessage ="Student name is required.")]
+        public string Name { get; set; }
+        [Required(ErrorMessage = "Class name is required.")]
+        public string Class { get; set; }
+    }
+}
