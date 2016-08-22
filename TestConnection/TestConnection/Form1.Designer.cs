da73189a46741eb17630ec487321f5c3864e64a6
govindaraj krishnan <gov777raj@yahoo.com>
2016-08-22 10:23:25 +0530

    Test Connection

---
+++ TestConnection/TestConnection/Form1.Designer.cs
@@ -0,0 +1,216 @@
+﻿namespace TestConnection
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.txtId = new System.Windows.Forms.TextBox();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.txtISU = new System.Windows.Forms.TextBox();
+            this.txtAN = new System.Windows.Forms.TextBox();
+            this.txtSkills = new System.Windows.Forms.TextBox();
+            this.txtRD = new System.Windows.Forms.TextBox();
+            this.txt_RP = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(40, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(48, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Emp_No";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(40, 51);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(62, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Emp_Name";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(40, 81);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(25, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "ISU";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(40, 113);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(81, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Account_Name";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(40, 140);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(45, 13);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Skillsets";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(40, 166);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(78, 13);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Released Date";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(40, 190);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(89, 13);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "Reporting Person";
+            // 
+            // txtId
+            // 
+            this.txtId.Location = new System.Drawing.Point(182, 22);
+            this.txtId.Name = "txtId";
+            this.txtId.Size = new System.Drawing.Size(225, 20);
+            this.txtId.TabIndex = 7;
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(182, 48);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(225, 20);
+            this.txtName.TabIndex = 8;
+            // 
+            // txtISU
+            // 
+            this.txtISU.Location = new System.Drawing.Point(182, 78);
+            this.txtISU.Name = "txtISU";
+            this.txtISU.Size = new System.Drawing.Size(225, 20);
+            this.txtISU.TabIndex = 9;
+            // 
+            // txtAN
+            // 
+            this.txtAN.Location = new System.Drawing.Point(182, 106);
+            this.txtAN.Name = "txtAN";
+            this.txtAN.Size = new System.Drawing.Size(225, 20);
+            this.txtAN.TabIndex = 10;
+            // 
+            // txtSkills
+            // 
+            this.txtSkills.Location = new System.Drawing.Point(182, 140);
+            this.txtSkills.Name = "txtSkills";
+            this.txtSkills.Size = new System.Drawing.Size(225, 20);
+            this.txtSkills.TabIndex = 11;
+            // 
+            // txtRD
+            // 
+            this.txtRD.Location = new System.Drawing.Point(182, 166);
+            this.txtRD.Name = "txtRD";
+            this.txtRD.Size = new System.Drawing.Size(225, 20);
+            this.txtRD.TabIndex = 12;
+            // 
+            // txt_RP
+            // 
+            this.txt_RP.Location = new System.Drawing.Point(182, 192);
+            this.txt_RP.Name = "txt_RP";
+            this.txt_RP.Size = new System.Drawing.Size(225, 20);
+            this.txt_RP.TabIndex = 13;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(142, 246);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(171, 23);
+            this.button1.TabIndex = 14;
+            this.button1.Text = "Insert Data";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(560, 300);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txt_RP);
+            this.Controls.Add(this.txtRD);
+            this.Controls.Add(this.txtSkills);
+            this.Controls.Add(this.txtAN);
+            this.Controls.Add(this.txtISU);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.txtId);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox txtId;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.TextBox txtISU;
+        private System.Windows.Forms.TextBox txtAN;
+        private System.Windows.Forms.TextBox txtSkills;
+        private System.Windows.Forms.TextBox txtRD;
+        private System.Windows.Forms.TextBox txt_RP;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ TestConnection/TestConnection/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace TestConnection
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public String conString = "Data Source=govindaraj\\SQLExpress;Initial Catalog=ConnectionDb;Integrated Security=True";
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection(conString);
+            con.Open();
+            if (con.State == System.Data.ConnectionState.Open)
+            {
+                string q = "insert into test(Emp_No, Emp_name, ISU, Account_Name, Skillsets, Released_Date, Reporting_Person) values('" + txtId.Text.ToString() + "', '" + txtName.Text.ToString() + "', '" + txtISU.Text.ToString() + "','" + txtAN.Text.ToString() + "','" + txtSkills.Text.ToString() + "','" + txtRD.Text.ToString() + "','" + txt_RP.Text.ToString() + "')";
+                SqlCommand cmd = new SqlCommand(q, con);
+                cmd.ExecuteNonQuery();
+                MessageBox.Show("Connection made successfully...!");
+             }
+            txtId.Text = "";
+            txtName.Text = "";
+            txtISU.Text = "";
+            txtAN.Text = "";
+            txtSkills.Text = "";
+            txtRD.Text = "";
+            txt_RP.Text = "";
+        }
+     }
+}
